e8dd16233e1e7b31931f7963de12bfab75c39d8b
AliaaHosny <aliaahosny77@gmail.com>
2016-06-29 15:30:29 -0700

    Adding pages

---
+++ js/custom.js
@@ -0,0 +1,76 @@
+
+ /* jQuery Preloader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* jQuery to collapse the navbar on scroll
+    -----------------------------------------------*/
+  $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
+  });
+
+
+
+   /* Owl Carousel
+    -----------------------------------------------*/
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
+  /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#work').parallax("100%", 0.3);
+    $('#about').parallax("100%", 0.2);
+    $('#team').parallax("100%", 0.3);
+    $('#portfolio').parallax("100%", 0.1);
+    $('#plan').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.2);
+  }
+  initParallax();
+
+
+  /* Nivo lightbox
+    -----------------------------------------------*/
+  $('#portfolio .col-md-4 a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
